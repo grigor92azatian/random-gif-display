b8b7925b24ede12901997fd1140728a22144818b
Grigor Azatian <34463837+grigor92azatian@users.noreply.github.com>
2021-11-04 20:28:59 -0700

    basic button functions working
    search button updates gif based on search term and new gif button reloads a new gif of the same category

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const img = document.querySelector('img');
 const newGIFButton = document.querySelector(".newGIFButton");
 const searchButton = document.querySelector(".searchButton");
+const inputField = document.querySelector(".inputField");
+let currentSearch = "cats";         //default search term
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=UuohM0uLy0H06vHX3Y4qAGTIXF6nHnfU&s=fish', {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  });
+getNewImg(currentSearch);           //initial img load
 
 newGIFButton.addEventListener("click", function(){
-
-});
+        getNewImg(currentSearch);
+    }
+);
 
 searchButton.addEventListener("click", function(){
-
+    currentSearch = getUserInput();
+    getNewImg(currentSearch);
 });
 
+function getNewImg(searchTerm){
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=UuohM0uLy0H06vHX3Y4qAGTIXF6nHnfU&s='+searchTerm, {mode: 'cors'})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(response) {
+        img.src = response.data.images.original.url;
+    });  
+}
+
+function getUserInput(){
+    //return users text input
+    return inputField.value;
+}
